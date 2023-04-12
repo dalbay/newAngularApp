508c3338814084cfc84b33033b3c32e64fe89d6c
dalbay <fadalbay@gmail.com>
2023-04-12 14:27:35 -0400

    unit test final

--- src/app/item/item.component.spec.ts
+++ src/app/item/item.component.spec.ts
@@ -3,39 +3,44 @@ import { ItemComponent } from './item.component';
 import { ItemService } from '../services/item.service';
 import { Observable, of } from 'rxjs';
 import { Item } from './item';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 describe('ItemComponent', () => {
-  let service;
   let component: ItemComponent;
   let fixture: ComponentFixture<ItemComponent>;
-  let items: Item[] = [
-    { title: '', description: '' },
-    { title: '', description: ''},
+  let itemService: ItemService;
+   let   items: Item[] = [
+    { title: 'car', description: 'vehicle' },
+    { title: 'ball', description: 'game'},
   ]
-  let mockService: any;
   beforeEach(async () => {
-    mockService = { getItems: () => { subscribe: () => {}  } }
-    
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, FormsModule, HttpClientModule],
       declarations: [ItemComponent],
+      schemas: [NO_ERRORS_SCHEMA],
       providers: [
-        { provide: ItemService, useValue: mockService }
-      ]
-    })
-    .compileComponents();
-    service = TestBed.inject(ItemService);
+        {
+          provide: ItemService, useValue: {
+            getItems: () => of(items)
+          }
+        }],
+    }).compileComponents();
+    
     fixture = TestBed.createComponent(ItemComponent);
     component = fixture.componentInstance;
+    itemService = TestBed.get(ItemService);
     fixture.detectChanges();
+    
   });
-
-  describe('should read items', () => {
-    it('should display a list of items', () => {
-      spyOn(mockService, 'getItems').and.returnValues(of(items))
-    component.ngOnInit();
-    fixture.detectChanges();
-    expect(component.items).toHaveSize(1);
+  
+    fit('should load a list of items', () => {
+      const spy = spyOn(itemService, 'getItems').and.callThrough();
+      component.ngOnInit();
+      fixture.detectChanges();
+      expect(itemService.getItems).toHaveBeenCalled();
+      expect(component.items.length).toBeGreaterThan(1);      
   });
-  })
-
 });
