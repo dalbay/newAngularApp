ead87a38d21ad79ea8971e9323c8426f8c923580
dalbay <fadalbay@gmail.com>
2023-04-12 13:00:18 -0400

    'unittest'

--- src/app/item/item.component.spec.ts
+++ src/app/item/item.component.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ItemComponent } from './item.component';
+import { ItemService } from '../services/item.service';
+import { Observable, of } from 'rxjs';
+import { Item } from './item';
 
 describe('ItemComponent', () => {
+  let service;
   let component: ItemComponent;
   let fixture: ComponentFixture<ItemComponent>;
-
+  let items: Item[] = [
+    { title: '', description: '' },
+    { title: '', description: ''},
+  ]
+  let mockService: any;
   beforeEach(async () => {
+    mockService = { getItems: () => { subscribe: () => {}  } }
+    
     await TestBed.configureTestingModule({
-      declarations: [ ItemComponent ]
+      declarations: [ItemComponent],
+      providers: [
+        { provide: ItemService, useValue: mockService }
+      ]
     })
     .compileComponents();
-
+    service = TestBed.inject(ItemService);
     fixture = TestBed.createComponent(ItemComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('should read items', () => {
+    it('should display a list of items', () => {
+      spyOn(mockService, 'getItems').and.returnValues(of(items))
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.items).toHaveSize(1);
   });
+  })
+
 });
